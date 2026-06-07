22081c8210132cd0527c303a867979a9137a0d55
Sameer thakur <wabmetacontact@gmail.com>
2026-06-08 00:37:21 +0530

    feat: integrate new Pricing component on the landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,6 +2,7 @@ import Navbar from '../components/landing/Navbar';
 import Hero from '../components/landing/Hero';
 import Features from '../components/landing/Features';
 import HowItWorks from '../components/landing/HowItWorks';
+import Pricing from '../components/landing/Pricing';
 
 const Landing = () => {
   return (
@@ -10,7 +11,7 @@ const Landing = () => {
       <Hero />
       <Features />
       <HowItWorks />
-      {/* Add more sections here */}
+      <Pricing />
     </div>
   );
 };
