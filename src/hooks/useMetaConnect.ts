925d4db8864be7e651c704496f8d618eb150b019
Sameer thakur <wabmetacontact@gmail.com>
2026-06-12 23:24:57 +0530

    fix: remove featureType to prevent hiding existing WABAs

--- src/hooks/useMetaConnect.ts
+++ src/hooks/useMetaConnect.ts
@@ -168,7 +168,7 @@ export const useMetaConnect = ({
           response_type: 'code',
           override_default_response_type: true,
           extras: {
-            featureType: 'whatsapp_business_app_onboarding',
+            featureType: '',
             sessionInfoVersion: '3',
             version: 'v3',
           },
